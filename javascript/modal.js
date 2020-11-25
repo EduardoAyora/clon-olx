9105699de8cca804c7cf6e1d837135771b32f4eb
EduardoAyora <eduardoaayora64@gmail.com>
2020-11-25 11:59:39 -0500

    fix: modals in expanded menu

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,22 +1,26 @@
 // Get the search modals
 var searchModals = document.getElementsByClassName("search-modal")
 // Get the button that opens the modal
-var btn = document.getElementById("search-button")
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    for (let index = 0; index < searchModals.length; index++) {
-        searchModals[index].style.display = "block"
+var btns = document.getElementsByClassName("search-button")
+// When the user clicks the button, open the modal
+for (let index = 0; index < btns.length; index++) {
+    btns[index].onclick = function() {
+        for (let index = 0; index < searchModals.length; index++) {
+            searchModals[index].style.display = "block"
+        }
     }
 }
 
 // Get the profile modals
 var profileModals = document.getElementsByClassName("profile-modal")
 // Get the button that opens the modal
-var btn = document.getElementById("profile-button")
+var btns = document.getElementsByClassName("profile-button")
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-    for (let index = 0; index < profileModals.length; index++) {
-        profileModals[index].style.display = "block"
+for (let index = 0; index < btns.length; index++) {
+    btns[index].onclick = function() {
+        for (let index = 0; index < profileModals.length; index++) {
+            profileModals[index].style.display = "block"
+        }
     }
 }
 
